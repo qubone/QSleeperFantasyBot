210cd91054aa4b4e968586e92f4d32a73cb175cd
Qubeman <linussbo@gmail.com>
2025-08-29 23:02:39 +0200

    Added Sleeper API parser and models

---
+++ src/qsleeperfantasybot/sleeper/model/avatar.py
@@ -0,0 +1,27 @@
+"""Handling of Sleeper Avatar data."""
+
+
+class Avatar:
+    """Users and leagues have avatar images.
+    There are thumbnail and full-size images for each avatar.
+    """
+
+    def __init__(self, avatar_id: str) -> None:
+        self._avatar_id = avatar_id
+        self._avatar_url = f"https://sleepercdn.com/avatars/{avatar_id}"
+        self._avatar_thumb_url = f"https://sleepercdn.com/avatars/thumbs/{avatar_id}"
+
+    @property
+    def avatar_id(self) -> str:
+        """Avatar ID."""
+        return self._avatar_id
+
+    @property
+    def avatar_url(self) -> str:
+        """Avatar URL."""
+        return self._avatar_url
+
+    @property
+    def avatar_thumb_url(self) -> str:
+        """Avatar thumbnail URL."""
+        return self._avatar_thumb_url
